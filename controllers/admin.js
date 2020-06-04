42061de94a1fff7d72dc6ff5a2e8e4cd26cce30e
187PM13983 - Phan Nguyen Thi Yen Nhi - K24T-PM1 <nhi.187pm13983@vanlanguni.vn>
2020-06-04 15:19:52 +0700

    modify log in - admin

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,23 @@
 var express = require('express');
 var router = express.Router();
-var MongoClient = require('mongodb').MongoClient;
-var uri = 'mongodb+srv://admin:tYFofQJbk98w31OR@cluster0-baxfc.mongodb.net/project';
+
+// var MongoClient = require('mongodb').MongoClient;
+// var uri = 'mongodb+srv://admin:tYFofQJbk98w31OR@cluster0-baxfc.mongodb.net/project';
+
+// middleware
+var multer = require('multer');
+var upload = multer({});
+// utils
+var MyUtil = require("../utils/MyUtil.js");
+var EmailUtil = require("../utils/EmailUtil.js");
+// daos
+//var pathDAO = "../daos/mongodb";
+var pathDAO = "../daos/mongodb";
+var AdminDAO = require(pathDAO + "/AdminDAO.js");
+var OrderDAO = require(pathDAO + "/OrderDAO.js");
+var CategoryDAO = require(pathDAO + "/CategoryDAO.js");
+var ProductDAO = require(pathDAO + "/ProductDAO.js");
+var CustomerDAO = require(pathDAO + "/CustomerDAO.js");
 
 //routes
 router.get('/', (req, resp) => {
@@ -20,27 +36,17 @@ router.get('/login', (req, resp) => {
     }
 });
 
-router.post('/login', (req, resp) => {
+router.post('/login', async (req, resp) => {
     var username = req.body.username;
     var password = req.body.password;
-    var crypto = require('crypto');
-    var pwdhash = crypto.createHash('md5').update(password).digest("hex");
-
-    MongoClient.connect(uri, (err, conn) => {
-        if(err) throw err;
-        var db = conn.db('project');
-        var query = { username: username, passwordhash: pwdhash };
-        db.collection('admins').findOne(query, (err, result) => {
-            if(err) throw err;
-            if(result) {
-                req.session.admin = result;
-                resp.redirect('home');
-            } else {
-                resp.redirect('login');
-            }
-            conn.close();
-        });
-    });
+    var pwdhashed = MyUtil.md5(password);
+    var admin = await AdminDAO.selectByUsernameAndPassword(username, pwdhashed);
+    if (admin) {
+      req.session.admin = admin;
+      resp.redirect('home');
+    } else {
+      MyUtil.showAlertAndRedirect(resp, 'Invalid login!', './login');
+    }
 });
 
 
