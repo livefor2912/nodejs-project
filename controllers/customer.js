d78bd89a2b6b6f7c4e17f10c9e97c05e6565bb12
187pm06722 <thy.187pm06722@vanlanguni.vn>
2020-06-04 17:22:50 +0700

    Update customer.js

--- controllers/customer.js
+++ controllers/customer.js
@@ -8,6 +8,29 @@ router.get('/', (req, resp) => {
 });
 
 
+router.get('/login', (req, resp) => {
+    if(req.session.cus) {
+        resp.redirect('home');
+    } else {
+        resp.render('../views/customer/login.ejs');
+    }
+});
+
+router.post('/login', async (req, resp) => {
+    var username = req.body.username;
+    var password = req.body.password;
+    var pwdhashed = MyUtil.md5(password);
+    var cus = await AdminDAO.selectByUsernameAndPassword(username, pwdhashed);
+    if (cus) {
+      req.session.cus = cus;
+      resp.redirect('home');
+    } else {
+      MyUtil.showAlertAndRedirect(resp, 'Invalid login!', './login');
+    }
+});
+
+
+
 
 
 module.exports = router;
