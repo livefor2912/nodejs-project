4ca5698fcb20a23a52a8905767e71b24a99d5faf
187pm06722 <thy.187pm06722@vanlanguni.vn>
2020-06-04 17:50:03 +0700

    Install express.session.memorystore

--- index.js
+++ index.js
@@ -9,10 +9,14 @@ app.use(express.static('public')); //say that all file in public is static, dont
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//init some var to provide cookie and session storage
+MemoryStore = express.session.MemoryStore
+var sessionStore = new MemoryStore();
+
 var session = require('express-session');
 app.use(session({ 
     secret: '123456',
-    name: cookie_name,
+    //name: cookie_name,
     store: sessionStore,
     proxy:true,
     resave: true,
