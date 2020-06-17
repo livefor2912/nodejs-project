8a46cfbfbc0a9b3484572943aa762bd838298f9d
LanPhuong <phuong.187pm13997@vanlanguni.vn>
2020-06-17 14:04:57 +0700

    my order customer

--- controllers/customer.js
+++ controllers/customer.js
@@ -6,9 +6,10 @@ var router = express.Router();
 
 var MyUtil = require("../utils/MyUtil.js");
 var multer = require('multer');
+const { ObjectID, ObjectId } = require('mongodb');
 
-// var pathDAO = "../daos/mongodb";
-var pathDAO = "../daos/mongoose";
+var pathDAO = "../daos/mongodb";
+//var pathDAO = "../daos/mongoose";
 var CategoryDAO = require(pathDAO + "/CategoryDAO.js");
 var ProductDAO = require(pathDAO + "/ProductDAO.js");
 var CustomerDAO = require(pathDAO + "/CustomerDAO.js");
@@ -36,19 +37,32 @@ router.get('/login', (req, resp) => {
 });
 
 router.post('/login', async (req, resp) => {
-    var username = req.body.username;
-    var password = req.body.password;
-    var pwdhashed = MyUtil.md5(password);
-    //var cus = await CustomerDAO.sele
-    var remember = req.body.remember;
-    var cus = CustomerDAO.selectByUsernameAndPassword(username, pwdhashed);
-    if (cus) {
+  var username = req.body.username;
+  var password = req.body.password;
+  // var pwdhashed = MyUtil.md5(password);
+  //var cus = await CustomerDAO.sele
+  var remember = req.body.remember;
+  var cus = await CustomerDAO.selectByUsernameAndPassword(username, password);
+  // var temp = CustomerDAO.test();
+  if (cus) {
       req.session.customer = cus;
       resp.redirect('/');
-    } else {
+  } else {
       MyUtil.showAlertAndRedirect(resp, 'Invalid login!', './login');
-    }
+  }
 });
 
-
+router.get('/myorders', async function (req, resp) {
+    var cust = req.session.customer;
+    if (cust) { 
+      var orders = await OrderDAO.selectByCustID(cust._id);
+      var _id = req.query.id; // /myorders?id=XXX
+      if (_id) {
+        var order = await OrderDAO.selectByID(_id);
+      } 
+      resp.render('../views/customer/myorders.ejs', { orders: orders, order: order});
+    } else {
+      resp.redirect('./');
+    }
+  });
 module.exports = router;
